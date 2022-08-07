28ffe8373ddb181e866ae1e5cd46f48cbb393394
LordPrimElo <evansrathore@gmail.com>
2022-08-07 11:08:43 +0530

    Add nodemailer functionality

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 // PACKAGE IMPORTS
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
+const nodeMailer = require("nodemailer")
+
+// Config CONFIG
+try {
+	var config = require("./config")
+} catch (e) {
+	console.log("Could not import config. Probable Cause: NOT WORKING LOCALLY, ya dum dum")
+	console.log(e)	
+}
+
 
 
 // ROUTE IMPORTS
@@ -10,6 +21,8 @@ const mainRoutes = require("./routes/main.js")
 // CONFIG
 app.set("view engine", "ejs")
 app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 
 // Route configuration
--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,38 @@
 const router = require("express").Router()
+const nodeMailer = require("nodemailer")
+
+try {
+	var config = require("../config")
+} catch (e) {
+	console.log("Could not import config. Probable Cause: NOT WORKING LOCALLY, ya dum dum")
+	console.log(e)	
+}
 
 router.get("/", (req, res) => {
+    res.render("index")
+})
+
+router.post("/", (req, res) => {
+    const sendMailToMe = async () => {
+        let transporter = nodeMailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false,
+            auth: {
+              user: "evansrathore@gmail.com",
+              pass: process.env.PASS || config.pass,
+            },
+          });
+        
+          let info = await transporter.sendMail({
+            from: req.body.email,
+            to: "evansrathore@gmail.com", 
+            subject: "Buisness EMAIL IMPORTANT",
+            text: req.body.emailText, 
+            html: req.body.emailText
+          });
+    }
+    sendMailToMe().catch(console.error)
     res.render("index")
 })
 
